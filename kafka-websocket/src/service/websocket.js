4415a3df31257ad401bbc8df852c3c30a2eee2f9
eslamdyab <eslamdyab22@gmail.com>
2024-07-10 19:29:22 +0300

    add new websocket2 for the home energy usage

--- kafka-websocket/src/service/websocket.js
+++ kafka-websocket/src/service/websocket.js
@@ -1,16 +1,25 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
+const wss2 = new WebSocketServer({ port: 9090 });
 
 const startWebSocketServers = async () => {
   wss.on("connection", (ws) => {
-    console.log("WebSocket server connected");
+    console.log("WebSocket 1 server connected");
   });
 
   wss.on("error", (error) => {
-    console.error("WebSocket server error:", error);
+    console.error("WebSocket 1 server error:", error);
+  });
+
+  wss2.on("connection", (ws) => {
+    console.log("WebSocket 2 server connected");
+  });
+
+  wss2.on("error", (error) => {
+    console.error("WebSocket 2 server error:", error);
   });
 
 };
 
-export { wss, startWebSocketServers };
+export { wss, wss2, startWebSocketServers };

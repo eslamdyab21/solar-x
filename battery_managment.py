8fb63f2d7c3e5b2ff4a17b5bb171ecfde36061bf
eslamdyab <eslamdyab22@gmail.com>
2024-07-12 23:15:13 +0300

    implement battery managment, charge part

---
+++ battery_managment.py
@@ -0,0 +1,116 @@
+import json
+import logging
+from Kafka_consumer import Kafka_consumer
+
+prev_home_consumption = None
+batteries_status = {}
+
+
+def load_batteries_info():
+    global batteries_status
+
+    with open('home_configurations.json') as f:
+        HOME_CONFIGURATIONS = json.load(f)
+
+    i = 1
+    for battery_capacity in HOME_CONFIGURATIONS['batteries_capacity_kwh']:
+        battery_name = 'battery_' + str(i)
+        if battery_name not in batteries_status:
+            batteries_status[battery_name] = {"capacity_kwh": battery_capacity, 
+                                             "max_charge_speed_w": HOME_CONFIGURATIONS['batteries_charge_max_speed_w_second'][i-1],
+                                             "current_energy_wh":battery_capacity*1000,
+                                             "is_charging":0}
+            i +=1
+
+
+
+
+def charge_batteries(access_power_w, last_battery_charged = None):
+    global batteries_status
+
+    min_energy_battery = 'battery_1'
+    min_energy = batteries_status[min_energy_battery]['current_energy_wh']
+    full_charged_counter = 0
+
+    for battery in batteries_status.keys():
+        if int(batteries_status[battery]['current_energy_wh']) == batteries_status[battery]['capacity_kwh']*1000:
+            full_charged_counter += 1
+
+        if batteries_status[battery]['current_energy_wh'] < min_energy and battery != last_battery_charged:
+            min_energy = batteries_status[battery]['current_energy_wh']
+            min_energy_battery = battery
+
+    if min_energy_battery == last_battery_charged or full_charged_counter == len(batteries_status.keys()):
+        return
+
+    if access_power_w > batteries_status[min_energy_battery]['max_charge_speed_w']:
+        access = access_power_w - batteries_status[min_energy_battery]['max_charge_speed_w']
+
+        access_power_w = batteries_status[min_energy_battery]['max_charge_speed_w']
+        batteries_status[min_energy_battery]['current_energy_wh'] += access_power_w
+        batteries_status[min_energy_battery]['is_charging'] = 1
+
+        if batteries_status[min_energy_battery]['current_energy_wh'] > batteries_status[battery]['capacity_kwh']*1000:
+            batteries_status[battery]['current_energy_wh'] = batteries_status[battery]['capacity_kwh']*1000
+            batteries_status[min_energy_battery]['is_charging'] = 0
+        else:    
+            charge_batteries(access_power_w = round(access,2), last_battery_charged = min_energy_battery)
+
+    else:
+        batteries_status[min_energy_battery]['current_energy_wh'] += access_power_w
+        batteries_status[min_energy_battery]['is_charging'] = 1
+
+        if batteries_status[min_energy_battery]['current_energy_wh'] > batteries_status[battery]['capacity_kwh']*1000:
+            batteries_status[battery]['current_energy_wh'] = batteries_status[battery]['capacity_kwh']*1000
+            batteries_status[min_energy_battery]['is_charging'] = 0
+
+
+
+def consume_batteries(access_power_w):
+    pass
+
+
+def bms(key, value):
+    global prev_home_consumption, batteries_status
+
+    if key == 'home_energy':
+        prev_home_consumption = value
+    elif prev_home_consumption != None:
+        access_power_w = value['current_consumption_w'] - prev_home_consumption['current_consumption_w']
+        if access_power_w > 0:
+            charge_batteries(round(access_power_w,2))
+            print(batteries_status)
+        else:
+            consume_batteries(-1*access_power_w)
+
+
+def main():
+    global batteries_status
+
+    consumer = Kafka_consumer(topic_name = ["solar_energy_data", "home_energy_consumption"])
+    consumer.kafka_consumer_conf(broker_address = "localhost:9092", 
+                                 consumer_group = "battery_proccessing",
+                                 auto_offset_reset = "latest")
+    
+    load_batteries_info()
+
+    while True:
+        msg = consumer.consume(timeout = 0.5)
+        if msg is None:
+            # print("Waiting....")
+            pass    
+        elif msg.error() is not None:
+            raise Exception(msg.error())
+        else:
+            key = msg.key().decode("utf-8")
+            value = json.loads(msg.value())
+            offset = msg.offset()
+
+            bms(key, dict(value))
+            # logging.debug(f"{offset} {key} {value}")
+            logging.debug("-------------------------------------------------")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level = "DEBUG")
+    main()

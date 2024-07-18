461d04a54e52ba64f9b7c0600cf6b8af78604c8c
eslamdyab <eslamdyab22@gmail.com>
2024-07-18 23:25:41 +0300

    add battery precentage to battery component

--- frontend-gui/src/components/battery/battery.jsx
+++ frontend-gui/src/components/battery/battery.jsx
@@ -2,6 +2,8 @@ import './battery.css'
 
 
 const Battery = (props) => {
+	let battery_precentage = 100 * props.batteryEnergyData.current_energy_wh	/ (props.batteryEnergyData.capacity_kwh*1000)
+
 	return(
 	    <div className="boxInfo">
 	        <div className='title'> 
@@ -9,14 +11,21 @@ const Battery = (props) => {
 	            <span>{props.batteryName}</span>
 	        </div>
 
-	        <div className='title-info'>
-	            {props.batteryEnergyData? 
-	                <h4> {props.batteryEnergyData.current_energy_wh.toLocaleString('en')} </h4>
-	                :
-	                <h5> Wating... </h5>
-	            }
-	             <h6  style={{ color: "gold" }}>Wh</h6>
-	        </div>
+	        <div>
+		        <div className='title-info'>
+		            {props.batteryEnergyData? 
+		                <h4> {props.batteryEnergyData.current_energy_wh.toLocaleString('en')} </h4>
+		                :
+		                <h5> Wating... </h5>
+		            }
+		             <h6  style={{ color: "gold" }}>Wh</h6>
+		        </div>
+
+		        <div className="battery_precentage" style={{ color: "limegreen" }}>
+	                <h4> {battery_precentage.toFixed(1)} % </h4>
+	            </div>
+            </div>
+
 	    </div>
 	)
 }

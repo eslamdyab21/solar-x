c52ed99916dd227559174cbbca64c90e01f328cc
eslamdyab <eslamdyab22@gmail.com>
2024-07-19 11:46:55 +0300

    make the check in the backend

--- frontend-gui/src/components/powerFlow/powerFlow.jsx
+++ frontend-gui/src/components/powerFlow/powerFlow.jsx
@@ -3,42 +3,12 @@ import SolarPowerOutlinedIcon from '@mui/icons-material/SolarPowerOutlined';
 import BroadcastOnHomeOutlinedIcon from '@mui/icons-material/BroadcastOnHomeOutlined';
 import CellTowerIcon from '@mui/icons-material/CellTower';
 import Battery from '../battery/battery'
-import { useState , useEffect} from 'react'
+import { useState } from 'react'
 import './powerFlow.css'
 
-let initial = true
-let prev_w, current_w = 0
-
-
-function battery_charge_x_consume (batteryEnergyData) {
-    let keys = Object.keys(batteryEnergyData);
-    let status
-    
-
-    keys.forEach((key) => {
-        if (key !== 'time_stamp' && initial){
-            prev_w = prev_w + batteryEnergyData[key]['current_energy_wh']
-            initial = false 
-        }
-        else if(key !== 'time_stamp'){
-            current_w = current_w +  batteryEnergyData[key]['current_energy_wh']
-        }
-    });
-
-    current_w - prev_w > 0 ? status = 'charging' 
-    : current_w - prev_w < 0 ? status = 'draining' : status = 'ideal' 
-
-    prev_w = current_w
-
-    return status
-}
-
-
-let status, prev_status = null
 
 const PowerFlow = (props) => {
     const [currentSolarProductionZeroFlag, setCurrentSolarProductionZeroFlagZeroFlag] = useState(false)
-    const [batteryStatus, setBatteryStatus] = useState('')
     let pass = false
 
 
@@ -51,18 +21,6 @@ const PowerFlow = (props) => {
         }
     }
     
-    
-
-    useEffect( () => {
-        setInterval(() => {
-            if(props.batteryEnergyData){
-                status = battery_charge_x_consume(props.batteryEnergyData)
-                console.log('batteryStatus: ', status)
-                status !== prev_status ? setBatteryStatus(status) : pass = true
-                prev_status = status
-            }
-        }, 5 *1000)
-    }, [])
 
     return(
         <div>
@@ -101,14 +59,14 @@ const PowerFlow = (props) => {
 
                 <div className='box_1row_1col_flow_pad'>
                     {props.solarEnergyData ? 
-                    <LinearIndeterminate animate={currentSolarProductionZeroFlag && batteryStatus === 'ideal'? true : false}/>
+                    <LinearIndeterminate animate={currentSolarProductionZeroFlag? true : false}/>
                     : 
                     <LinearIndeterminate animate={false}/>} 
                 </div>
 
 
 
-                {currentSolarProductionZeroFlag && batteryStatus === 'ideal'? 
+                {currentSolarProductionZeroFlag? 
                 <div className='box_flow box_1row_1col_flow_grid_power_icon'> 
                     <div className='grid_power_icon_black'>
                         <CellTowerIcon fontSize='large' sx={{ fontSize: 50 }} />
@@ -123,7 +81,7 @@ const PowerFlow = (props) => {
                 }
             </div>
 
-            <div className='flex-batteries-container-active' sx={{ fontSize: 5000 }}>
+            <div className='flex-batteries-container-active'>
                 <div className='flex-battery'>
                 { props.batteryEnergyData?
                     <Battery batteryEnergyData={props.batteryEnergyData.battery_1} batteryName={'Battery 1'} icon={props.icon}/>
@@ -146,10 +104,8 @@ const PowerFlow = (props) => {
                     <h5>Waiting....</h5>
                 }
                 </div>
-
             </div>
         </div>
-        
     )
 }
 

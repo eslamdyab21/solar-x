874e397b70d0965923aca5f3249c1c9423550843
eslamdyab <eslamdyab22@gmail.com>
2024-07-31 11:55:27 +0300

    rename

---
+++ mysql_database/Database.py
@@ -0,0 +1,62 @@
+import mysql.connector
+from dotenv import load_dotenv
+import time
+import os
+
+
+class Database():
+
+    def __init__(self):
+        self.connect_to_db()
+
+
+
+    def connect_to_db(self):
+        
+
+        load_dotenv()
+        ENV_DATABASE_USER = os.getenv('DATABASE_USER')
+        ENV_DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+        ENV_DATABASE_HOST = os.getenv('DATABASE_HOST')
+        ENV_DATABASE_NAME = os.getenv('DATABASE_NAME')
+        ENV_DATABASE_PORT = os.getenv('DATABASE_PORT')
+
+        self.connection = mysql.connector.connect(
+            user=ENV_DATABASE_USER,
+            password=ENV_DATABASE_PASSWORD,
+            port=ENV_DATABASE_PORT,
+            host=ENV_DATABASE_HOST,
+            database=ENV_DATABASE_NAME
+        )
+
+        self.cursor = self.connection.cursor()
+        print(f'Database : connect_to_db ' + "Connection is done")
+        # self.cursor.execute(f"""USE {ENV_DATABASE_NAME}""")
+        print('Database --> ' + f"{ENV_DATABASE_NAME} Database is in use")
+
+
+    def select_query(self, query):
+
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+
+        
+        return result
+
+
+
+    def insert_query(self, query):
+
+        self.cursor.execute(query)
+        self.connection.commit()
+
+        
+
+
+
+    def update_query(self, query):
+
+        self.cursor.execute(query)
+        self.connection.commit()
+
+
---
+++ mysql_database/Kafka_consumer.py
@@ -0,0 +1,27 @@
+from quixstreams import Application
+
+class Kafka_consumer():
+    def __init__(self, topic_name):
+        self.topic_name = topic_name
+
+        
+
+    def kafka_consumer_conf(self, broker_address, consumer_group, auto_offset_reset):
+        self.app = Application(
+            broker_address = broker_address,
+            loglevel = "DEBUG",
+            consumer_group = consumer_group,
+            auto_offset_reset = auto_offset_reset
+        )
+
+        self.consumer = self.app.get_consumer()
+        self.consumer.subscribe(self.topic_name)
+
+    
+    def consume(self, timeout):
+
+        msg = self.consumer.poll(timeout)
+        if msg:
+            self.consumer.store_offsets(msg)
+
+        return msg

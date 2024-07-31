dfbc709232d15228451347c50d60c79623f5ba3e
eslamdyab <eslamdyab22@gmail.com>
2024-07-31 14:34:36 +0300

    fix is_day paramter

--- weather_producer_simulation.py
+++ weather_producer_simulation.py
@@ -7,11 +7,21 @@ from Kafka_producer import Kafka_producer
 
 msg = {'current':{'is_day':1, 'cloud_cover':random.randint(100,1000)/1000,
                   'wind_speed_10m':20, 'temperature_2m':35, 'time_stamp':0}, 
-       'daily':{'sunrise':['2024-07-09T05:58:58'], 'sunset':['2024-07-09T20:58:58']}}
+       'daily':{'sunrise':['2024-07-09T05:58:58'], 'sunset':['2024-07-09T18:58:58']}}
 
 def get_weather():
-    time_stamp = str(datetime.datetime.now().replace(microsecond=0))
-    msg['time_stamp'] = time_stamp
+    time_stamp = datetime.datetime.now().replace(microsecond=0)
+
+    msg['current']['time_stamp'] = str(time_stamp)
+    msg['daily']['sunrise'][0] = msg['daily']['sunrise'][0].replace('2024-07-09', str(time_stamp).split()[0])  
+    msg['daily']['sunset'][0] = msg['daily']['sunset'][0].replace('2024-07-09', str(time_stamp).split()[0])  
+    msg['current']['cloud_cover'] = random.randint(100,1000)/1000
+
+    if time_stamp > datetime.datetime.fromisoformat(msg['daily']['sunrise'][0]) and time_stamp < datetime.datetime.fromisoformat(msg['daily']['sunset'][0]):
+        msg['current']['is_day'] = 1
+    else:
+        msg['current']['is_day'] = 0
+
 
     return msg
 

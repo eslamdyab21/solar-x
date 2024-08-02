c1f8a46a3d2bc88ab45f6509924b09770ec5cb1c
eslamdyab <eslamdyab22@gmail.com>
2024-08-02 15:27:51 +0300

    fix hourly data date typo

--- bms.py
+++ bms.py
@@ -56,8 +56,8 @@ class BMS():
         current_hour = time_stamp.split()[1].split(':')[0]
 
         if int(current_hour) == 0 and self.reset_hourly_discharging_flag:
-            self.batteries_status['hourly_discharging'] = {"00":0,"01":0,"02":0,"03":0,"04":0,"05":0,"06":0,
-                                                            "07":0,"08":0,"09":0,"10":0,"11":0,"12":0,"13":0,
+            self.batteries_status['hourly_discharging'] = {"0":0,"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,
+                                                            "7":0,"8":0,"9":0,"10":0,"11":0,"12":0,"13":0,
                                                             "14":0,"15":0,"16":0,"17":0,"18":0,"19":0,"20":0,
                                                             "21":0,"22":0,"23":0}
             self.reset_hourly_discharging_flag = False

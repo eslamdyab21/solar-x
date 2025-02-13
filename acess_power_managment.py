ebd81b3a75b2f762a7315eb46a594d0a5a6f5fe1
eslamdyab21 <eslamdyab22@gmail.com>
2025-02-14 01:39:55 +0200

    make sure solar data and home are in the same time stamp

--- acess_power_managment.py
+++ acess_power_managment.py
@@ -7,32 +7,70 @@ from Kafka_consumer import Kafka_consumer
 from Kafka_producer import Kafka_producer
 from bms import BMS
 
-prev_home_consumption = None
-def access_power_managment(key, value, bms):
-    global prev_home_consumption
 
-    if key == 'home_energy':
-        prev_home_consumption = value
+def access_power_management(solar_msg, home_msg, bms):
+
+    access_power_w = solar_msg['current_consumption_w'] - home_msg['current_consumption_w']
+
+    if access_power_w > 0:
+        access_power_from_batteries = bms.charge_batteries(round(access_power_w,2))
+
+        if access_power_from_batteries:
+            # charge the national grid
+            pass
+
+    else:
+        negative_access_power_from_batteries = bms.discharge_batteries(-1*access_power_w)
+
+        if negative_access_power_from_batteries:
+            # consume from the national grid
+            pass
 
 
-    # normal operation
-    elif prev_home_consumption != None:
+solar_energy_stack = []
+home_energy_stack  = []
+def data_of_same_second(key, msg, bms):
 
-        access_power_w = value['current_consumption_w'] - prev_home_consumption['current_consumption_w']
+    if key == "solar_w":
+        current_solar_msg = msg
 
-        if access_power_w > 0:
-            access_power_from_batteries = bms.charge_batteries(round(access_power_w,2))
+        if len(home_energy_stack) == 0:
+            solar_energy_stack.append(current_solar_msg)
+            return False
+        
+        last_home_energy_msg = home_energy_stack[-1]
 
-            if access_power_from_batteries:
-                # charge the national grid
-                pass
+        if current_solar_msg["time_stamp"] == last_home_energy_msg["time_stamp"]:
+            del home_energy_stack[-1]
+
+            access_power_management(current_solar_msg, last_home_energy_msg, bms) 
+            return True
 
         else:
-            negative_access_power_from_batteries = bms.discharge_batteries(-1*access_power_w)
+            solar_energy_stack.append(current_solar_msg)
+            return False
 
-            if negative_access_power_from_batteries:
-                # consume from the national grid
-                pass
+
+    elif key == "home_energy":
+        current_home_msg = msg
+
+        if len(solar_energy_stack) == 0:
+            home_energy_stack.append(current_home_msg)
+            return False
+        
+        last_solar_msg = solar_energy_stack[-1]
+
+        if current_home_msg["time_stamp"] == last_solar_msg["time_stamp"]:
+            del solar_energy_stack[-1]
+
+            access_power_management(last_solar_msg, current_home_msg, bms)
+            return True
+        
+        else:
+            home_energy_stack.append(current_home_msg)
+            return False
+
+
 
 
 def main():
@@ -49,27 +87,33 @@ def main():
 
     bms = BMS()
     while True:
-        msg = consumer.consume(timeout = 0.5)
+        msg = consumer.consume(timeout = 1.0)
+
         if msg is None:
-            # print("Waiting....")
+            print("Waiting....")
             pass    
+
         elif msg.error() is not None:
             raise Exception(msg.error())
+        
         else:
             key = msg.key().decode("utf-8")
             value = json.loads(msg.value())
             offset = msg.offset()
 
-            access_power_managment(key, dict(value), bms)
+            logging.debug(f"{offset} {key} {value}")
 
-            msg = bms.batteries_status.copy()
-            msg['time_stamp'] = value['time_stamp']
+            if data_of_same_second(key, dict(value), bms):
+                # access_power_managment(key, dict(value), bms)
 
-            producer.kafka_produce(message_value = msg)
-            logging.debug(f"{msg}")
+                msg = bms.batteries_status.copy()
+                msg['time_stamp'] = value['time_stamp']
 
-            # logging.debug(f"{offset} {key} {value}")
-            logging.debug("-------------------------------------------------")
+                producer.kafka_produce(message_value = msg)
+                logging.debug(f"{msg}")
+
+                # logging.debug(f"{offset} {key} {value}")
+                logging.debug("-------------------------------------------------")
 
 
 
